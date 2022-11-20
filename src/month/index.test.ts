003cd77e7b303980907caf3be9aa637b0d17f235
Mathieu Decaffmeyer <5883963+mathieuprog@users.noreply.github.com>
2022-11-20 23:14:15 +0700

    Add tests

---
+++ src/month/index.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from 'vitest';
+import { Temporal } from '@js-temporal/polyfill';
+import { getMonthGrid } from '.';
+import DayOfWeek from '../day-of-week/DayOfWeek';
+
+test('getMonthGrid', () => {
+  let yearMonth: Temporal.PlainYearMonth;
+  let grid;
+
+  yearMonth = Temporal.PlainYearMonth.from({ year: 2022, month: 10 });
+  grid = getMonthGrid(yearMonth);
+
+  expect(grid.length).toBe(6);
+  expect(grid[0].length).toBe(7);
+
+  expect(Temporal.PlainDate.compare(grid[0][0].date, Temporal.PlainDate.from('2022-09-26'))).toBe(0);
+  expect(grid[0][0].chronology).toBe('before');
+
+  yearMonth = Temporal.PlainYearMonth.from({ year: 2022, month: 10 });
+  grid = getMonthGrid(yearMonth, DayOfWeek.Sunday);
+
+  expect(Temporal.PlainDate.compare(grid[0][0].date, Temporal.PlainDate.from('2022-09-25'))).toBe(0);
+  expect(grid[0][0].chronology).toBe('before');
+
+  yearMonth = Temporal.PlainYearMonth.from({ year: 2022, month: 10 });
+  grid = getMonthGrid(yearMonth, DayOfWeek.Saturday);
+
+  expect(Temporal.PlainDate.compare(grid[0][0].date, Temporal.PlainDate.from('2022-10-01'))).toBe(0);
+  expect(grid[0][0].chronology).toBe('during');
+});
