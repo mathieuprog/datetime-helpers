66356d871be5cd61805289c8d49198baa3666aa9
Mathieu Decaffmeyer <5883963+mathieuprog@users.noreply.github.com>
2022-11-22 19:11:10 +0700

    Add tests

--- src/month/index.test.ts
+++ src/month/index.test.ts
@@ -14,17 +14,20 @@ test('getMonthGrid', () => {
   expect(grid[0].length).toBe(7);
 
   expect(Temporal.PlainDate.compare(grid[0][0].date, Temporal.PlainDate.from('2022-09-26'))).toBe(0);
+  expect(grid[0][0].dateIsoString).toBe('2022-09-26');
   expect(grid[0][0].chronology).toBe('before');
 
   yearMonth = Temporal.PlainYearMonth.from({ year: 2022, month: 10 });
   grid = getMonthGrid(yearMonth, DayOfWeek.Sunday);
 
   expect(Temporal.PlainDate.compare(grid[0][0].date, Temporal.PlainDate.from('2022-09-25'))).toBe(0);
+  expect(grid[0][0].dateIsoString).toBe('2022-09-25');
   expect(grid[0][0].chronology).toBe('before');
 
   yearMonth = Temporal.PlainYearMonth.from({ year: 2022, month: 10 });
   grid = getMonthGrid(yearMonth, DayOfWeek.Saturday);
 
   expect(Temporal.PlainDate.compare(grid[0][0].date, Temporal.PlainDate.from('2022-10-01'))).toBe(0);
+  expect(grid[0][0].dateIsoString).toBe('2022-10-01');
   expect(grid[0][0].chronology).toBe('during');
 });
